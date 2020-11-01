7a8c53a415ba925e15f0680d5a617f666a309249
Amiruddin <joko.it-consultant@prudential.co.id>
2020-11-01 11:54:29 +0700

    add vue

---
+++ todovue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Todo />
+  </div>
+</template>
+
+<script>
+import Todo from './components/Todo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Todo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ todovue/src/components/Todo.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <input type="text" v-model="inputTodo"/>
+        <button v-on:click="addTodo">Add Todo</button>
+        <ol>
+            <li v-for="(todo, index) in todoList" :key="index" v-on:click="complete(index)">
+                <s v-if="todo.completed">{{ todo.text }}</s>
+                <span v-if="!todo.completed">{{ todo.text }}</span>
+            </li>
+        </ol>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Todo',
+        data: function(){
+            return{
+                inputTodo: '',
+                todoList: []
+            }
+        },
+        methods:{
+            addTodo: function(){
+                const newTodo = {
+                    text: this.inputTodo,
+                    completed: false,
+                }
+                this.todoList.push(newTodo)
+            },
+            complete: function(index){
+                this.todoList[index].completed = true;
+            }
+        }
+    }
+</script>
+
